66d9cf561e779b8421c97c169203026c9c5dda2d
Chris Lung <chrislung@Chriss-MBP.verizon.net>
2020-12-17 21:32:53 -0500

    inputbox functional with state

--- src/components/vocabAPI.jsx
+++ src/components/vocabAPI.jsx
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
+import React, { useState, Component } from 'react'
 
 function vocabAPI() {
+  // React Hooks
+  const [vocab, setVocab] = useState('')
+
+function handleVocab (e) {
+  setVocab(e.target.value);
+}
+ 
   return (
     <div className='vocabContainer'>
       This is our API Component!
-      <form> {/* onSubmit={this.handleSubmitVocab} */}
+      <form> {/* onSubmit={handleSubmitVocab} */}
         <label>
-          <input type="text" name="vocab" placeholder="Translate vocabulary"></input> {/* onChange={this.handleVocab} */}
+          <input type="text" name="vocab" placeholder="Translate vocabulary" value={vocab} onChange={handleVocab}></input> {/* onChange={this.handleVocab} */}
         </label>
         <input type="submit" value="Submit"/> 
       </form>
