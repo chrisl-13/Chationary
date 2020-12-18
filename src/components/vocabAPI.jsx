5b253de0199e630fa916fd8b67fe3d6930cd46da
Chris Lung <chrislung@Chriss-MBP.verizon.net>
2020-12-17 22:38:15 -0500

    Added 10 vocab limit to vocab history when rendering

--- src/components/vocabAPI.jsx
+++ src/components/vocabAPI.jsx
@@ -1,21 +1,28 @@
 import React, { useState, Component } from 'react'
 
 function VocabAPI() {
-  // React Hooks
+  // React Hooks State
   const [vocab, setVocab] = useState('');
-  const [vocabHist, setVocabHist] = useState(['apple', 'banana']);
+  const [vocabHist, setVocabHist] = useState([' ', 'apple', ' ', 'banana']);
 
+  // React Hooks Functions
   const handleVocab = (e) => {
     setVocab(e.target.value);
   }
 
   const handleSubmitVocab = (e) => {
-    e.preventDefault();
+    e.preventDefault(); //Prevents hot reload upon submit
     const currSearch = e.target[0].value;
-    setVocabHist([currSearch, ...vocabHist]);
+    if (vocabHist.length <= 18) {
+      setVocabHist([' ', currSearch, ...vocabHist]);
+    } else {
+      const vocabHistCopy = vocabHist.slice(0, vocabHist.length - 2);
+      setVocabHist([' ', currSearch, ...vocabHistCopy]);
+    }
     console.log('Form Submitted');
   }
  
+  //Render
   return (
     <div className='vocabContainer'>
       This is our API Component!
